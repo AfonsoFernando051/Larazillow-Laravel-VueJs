45389beee1029eba47ff3ca6cfd108a267e21448
AfonsoFernando051 <afonsofernando051@gmail.com>
2023-11-25 23:12:36 -0300

    first commit

---
+++ larazillow/resources/js/Pages/Index/Index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        Index
+    </div>
+    <Link href="/hello">
+        show
+    </Link>
+    <div>
+        The message is {{ message }}
+    </div>
+</template>
+
+<script setup>
+    import {Link} from '@inertiajs/vue3'
+    defineProps({
+        message: String
+    })
+</script>
